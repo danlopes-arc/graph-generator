cc1574109903993b76d4e21601503138e4efb133
Daniel Lopes <danlopes-arc>
2023-02-23 12:34:42 -0500

    Create Pixi Application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useRef } from "react";
+import * as PIXI from 'pixi.js'
+
 export const App: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const app = new PIXI.Application({
+      background: '#1099bb',
+      width: 100,
+      height: 100,
+    })
+
+    const view = app.view as HTMLCanvasElement
+
+    const container = containerRef.current
+
+    container?.appendChild(view)
+
+    return () => {
+      container?.removeChild(view)
+      app.destroy()
+    }
+  }, [])
+
+
   return (
-    <div>hey!</div>
+    <div ref={containerRef}></div>
   );
 }
